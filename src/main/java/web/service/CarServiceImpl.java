eb4b2bdf1b2ee5ae1ccf58265b6d856de3e0ff77
DariaStarintseva <yudina.daria@inbox.ru>
2023-08-23 09:32:34 +0300

    2.2.2

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,11 +1,7 @@
 package web.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import web.dao.CarDao;
 import web.model.Car;
 import java.util.List;
